d461f36c3612ea9798e1d2a5295a7bafc5002587
Juan Menendez <juan.menendez.buitrago@gmail.com>
2024-04-02 18:45:27 -0500

    Añade efectos y asocia a los modals si los tiene

--- src/components/ButtonWithIcon.vue
+++ src/components/ButtonWithIcon.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="button-with-icon" :class="{flat, selected, simple, withFilters: filterCount > 0}">
-        <Config v-if="icon=='filter'" style="margin-right: 0.5rem"/>
-        <ArrowLeft v-if="icon=='left'" style="margin-right: 0.5rem"/>
-        {{ text + (filterCount > 0 ? `(${filterCount})` : '') }}
-        <ArrowRight v-if="icon=='right'" style="margin-left: 0.5rem"/>
-        <ChevronDown v-if="icon=='down'" style="margin-left: 0.5rem" :selected="selected"/>
-        <ChevronUp v-if="icon=='up'" style="margin-left: 0.5rem" selected="selected"/>
+    <div style="position: relative;">
+        <div class="dropdown-button" 
+            :class="{ flat, selected, simple, withFilters: count > 0 }" 
+            :ref="reference"
+            @click.stop="emitEvent('clicked')">
+            <Config v-if="icon == 'filter'" style="margin-right: 0.5rem" />
+            <ArrowLeft v-if="icon == 'left'" style="margin-right: 0.5rem" />
+            {{ text + (count > 0 ? `(${count})` : '') }}
+            <ArrowRight v-if="icon == 'right'" style="margin-left: 0.5rem" />
+            <ChevronDown v-if="icon == 'down'" style="margin-left: 0.5rem" :selected="selected" />
+            <ChevronUp v-if="icon == 'up'" style="margin-left: 0.5rem" selected="selected" />
+        </div>
+        <slot></slot>
     </div>
 </template>
 
@@ -17,13 +23,19 @@ import ArrowRight  from '~/components/icons/ArrowRight.vue';
 import ArrowLeft   from '~/components/icons/ArrowLeft.vue';
 
 export default {
-    components:{
+    data(){
+        return {
+            top:0,
+        }
+    },
+    components: {
         ChevronDown,
         ChevronUp,
         Config,
         ArrowLeft,
         ArrowRight
     },
+ 
     props: {
         simple: {
             type: Boolean,
@@ -40,11 +52,15 @@ export default {
             required: false,
             default: false
         },
+        reference: {
+            type: String,
+            required: false,
+        },
         text: {
             type: String,
             required: true,
         },
-        filterCount:{
+        count: {
             type: Number,
             required: false,
         },
@@ -56,66 +72,84 @@ export default {
                 return ['down', 'up', 'left', 'right', 'filter'].includes(value);
             }
         }
-    }
+    },
 
+    methods:{
+        emitEvent(type)  {
+            this.$emit(type);
+        }
+    }
 }
 </script>
 
 <styles scoped lang="scss">
-    @import '~/assets/variables.scss';
-    .left, .right{
+@import '~/assets/variables.scss';
+
+.left,
+.right {
+    svg {
+        path {
+            fill: $orange;
+        }
+    }
+}
+
+.dropdown-button {
+    transition: all .2s ease-in;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    border: 1px solid $gray;
+    border-radius: 3px;
+
+    padding: 0.3rem 0.8rem;
+    cursor: pointer;
+    color: $black;
+
+    svg {
+        width: 1.25rem;
+        height: auto;
+
+        path {
+            fill: $black;
+            transition: all .2s ease-in;
+        }
+    }
+
+    &:hover {
+        color: $orange;
+        path{
+            fill: $orange;
+        }
+    }
+
+
+
+    &.selected {
+        border-color: $orange;
+        background-color: $orange-light;
+        color: $orange;
+
         svg {
-            path{
+            path {
                 fill: $orange;
             }
         }
     }
-    
-    .button-with-icon{
-        display: flex;
-        flex-direction: row;
-        align-items: center;
 
-        border: 1px solid $gray;
-        border-radius: 3px;
-
-        padding: 0.3rem 0.8rem;
-        margin-right: 0.5rem;
-        cursor: pointer;
-        color: $black;
-
-        svg{
-            width: 1.25rem;
-            height: auto;
-            path{
-                fill: $black;
-            }
-        }
-
-        
-        &.selected{
-            border-color: $orange;
-            background-color: $orange-light;
-            color: $orange;
-            svg {
-                path{
-                    fill: $orange;
-                }
-            }
-        }
-        
-        &.flat{
-            border: none;
-        }
-        
-        &.simple{
-            background-color: transparent;
-        }
-        
-        &.withFilters{
-            font-weight: 600;
-        }
-        
+    &.flat {
+        border: none;
     }
-    
+
+    &.simple {
+        background-color: transparent;
+    }
+
+    &.withFilters {
+        font-weight: 600;
+    }
+
+}
 </styles>
