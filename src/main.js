ea1947361c9bef29336cf6bd2224fb05fd938a4d
Juan Menendez <juan.menendez.buitrago@gmail.com>
2024-04-06 13:20:48 -0500

    Directiva para implementar click outside

--- src/main.js
+++ src/main.js
@@ -2,18 +2,29 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import '~/assets/styles.scss'
-import DefaultLayout from '~/layouts/Default.vue'
-import store from './data/store' 
+import DefaultLayout from '~/layouts/Default.vue';
+import store         from './data/store' ;
+import VueFuse       from 'vue-fuse';
+import data          from './data/house.json';
 
 export default function (Vue, { router, head, isClient }) {
-  // head.script.push({
-  //   src: './static/gmaps.js',
-  //   body: true,
-  //   type: 'text/javascript'
-
-  // })
-  // Set default layout as a global component
+  Vue.use(VueFuse);
   Vue.component('Layout', DefaultLayout);
-  // Register Vuex store
+  Vue.directive('click-outside', {
+    bind: function (el, binding, vnode) {
+      el.clickOutsideEvent = function (event) {
+        if (!(el == event.target || el.contains(event.target))) {
+          vnode.context[binding.expression](event);
+        }
+      };
+      document.body.addEventListener('click', el.clickOutsideEvent)
+    },
+    unbind: function (el) {
+      document.body.removeEventListener('click', el.clickOutsideEvent)
+    },
+  });
+
+
   Vue.prototype.$store = store
+  Vue.prototype.$realStateData = data
 }
